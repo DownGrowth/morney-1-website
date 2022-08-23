df6879f1542bddbe46480bcf1cbe138d27b48df3
BK <1209157041@qq.com>
2022-08-23 15:57:03 +0800

    deploy

---
+++ precache-manifest.6aa9f8e9b81c9b837afe1088d064020f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "148bd0c435f1c15bcccd",
+    "url": "/morney-1-website/css/app.a3a5ca58.css"
+  },
+  {
+    "revision": "97df063b9c992c8a317fbc8ad6080505",
+    "url": "/morney-1-website/index.html"
+  },
+  {
+    "revision": "148bd0c435f1c15bcccd",
+    "url": "/morney-1-website/js/app.d9c4c0f9.js"
+  },
+  {
+    "revision": "6d0eb448449f0d1a4aa0",
+    "url": "/morney-1-website/js/chunk-vendors.6376a34d.js"
+  },
+  {
+    "revision": "d1372d61b03359d367fb319fe20d26c9",
+    "url": "/morney-1-website/manifest.json"
+  },
+  {
+    "revision": "11addba782fc51900aad05be36d91e94",
+    "url": "/morney-1-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-1-website/robots.txt"
+  }
+]);
